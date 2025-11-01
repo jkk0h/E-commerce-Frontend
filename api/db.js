73710ca17c09074b11b891302c80bb6ec92a9ea0
Creutz-feldt <cjzheng2154@gmail.com>
2025-11-01 16:01:43 +0800

    Update db.js

--- api/db.js
+++ api/db.js
@@ -11,7 +11,7 @@ export async function connectAll() {
 
   // 👇 CHANGE THIS LINE
   // Use MONGO_URL, which Railway automatically sets, instead of MONGODB_URI
-  const mongoClient = new MongoClient(process.env.MONGO_URL); 
+  const mongoClient = new MongoClient(process.env.MONGO_URI); 
   await mongoClient.connect();
   const mongoDb = mongoClient.db(process.env.MONGO_DB_NAME || "appdb");
 
