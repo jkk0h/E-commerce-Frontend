60790f65b4c525ae9e2ecf35c26fd6270615b236
jkk0h <jiakaikoh@hotmail.com>
2025-11-09 19:55:45 +0800

    Changed from local to api address

--- frontend/script/config.js
+++ frontend/script/config.js
@@ -1,4 +1,4 @@
-window.API_URL = "http://localhost:3001/";
+window.API_URL = "https://e-commerce-frontend-production-f9c0.up.railway.app/";
 window.getApiUrl = (path = "") =>
     (window.API_URL.endsWith("/") ? window.API_URL : window.API_URL + "/") +
     (path.startsWith("/") ? path.slice(1) : path);
