8430ebde7034846f19d39483104bcf8ce0bab7c7
Benyuangiegie <113953592+Benyuangiegie@users.noreply.github.com>
2025-11-18 18:44:05 +0800

    Delete api/index.js

--- api/index.js
+++
@@ -1,7 +0,0 @@
-import { createServer } from "./app.js";
-import { PORT } from "./config/env.js";
-
-const app = createServer();
-app.listen(PORT, () => {
-  console.log(`✅ API listening on http://localhost:${PORT}`);
-});
