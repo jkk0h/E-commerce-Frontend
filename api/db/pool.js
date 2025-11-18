66449713c8cd2912ce2e030d316db816444b835d
Benyuangiegie <113953592+Benyuangiegie@users.noreply.github.com>
2025-11-18 18:47:00 +0800

    Delete api/db directory

--- api/db/pool.js
+++
@@ -1,7 +0,0 @@
-import pg from "pg";
-import { POSTGRES_URL, shouldUseSsl } from "../config/env.js";
-
-export const pool = new pg.Pool({
-  connectionString: POSTGRES_URL,
-  ssl: shouldUseSsl() ? { rejectUnauthorized: false } : false,
-});
