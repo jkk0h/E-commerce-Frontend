671cad6cccabe754030f53499330fa18561bf4ab
Benyuangiegie <113953592+Benyuangiegie@users.noreply.github.com>
2025-11-18 18:46:49 +0800

    Delete api/middleware directory

--- api/middleware/error.js
+++
@@ -1,7 +0,0 @@
-export function errorHandler(err, req, res, _next) {
-  console.error("ERROR:", err);
-  const status = err.statusCode || 500;
-  res.status(status).json({
-    error: err.message || "Unexpected error",
-  });
-}
